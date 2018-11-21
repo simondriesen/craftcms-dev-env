a1ad77ff1c1dd5d5a1e8596e0f53cf930114b7af
Simon Driesen <simon.driesen@hotmail.com>
2018-11-21 16:50:41 +0100

    Potential permission fix

--- startup.sh
+++ startup.sh
@@ -12,7 +12,7 @@ if ! grep -qs '/dev/sda1 /www' /proc/mounts; then
   # Volume is mounted.
   if ! [ "$(ls -A /www)" ]; then
     echo "Downloading latest craft cms version..."
-    composer create-project craftcms/craft /www && chown -R www:www /www
+    composer create-project craftcms/craft /www && chmod -R 774 /www && chown -R root:www /www
     echo "Craft CMS is ready for installation!"
     echo "I recommend that you install craft through the command line rather than through the web, the installation on the web may fail after a minute because of mysql's default timeout settings (depending on your mysql setup)."
   fi
